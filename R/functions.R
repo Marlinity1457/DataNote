62e3372f18aca51c9016234102808c4aff3b8457
Marleen van Doorn <m.c.vandoorn@erasmusmc.nl>
2023-08-29 10:56:37 +0200

    start with dates and time functions

--- R/functions.R
+++ R/functions.R
@@ -2,18 +2,28 @@
 
 # strsplit
 # unlist
+# tolower
 # lapply
 # sapply
 # vapply
 
 #--- functions which are available ---
-# sort
 # print
 # identical
 # round
 # abs
 # sum
 # mean
+# c
+# list
+# seq(start,end, by = 1)                Make a sequence
+# rep(c(1,2,3,4), times, each)          Replication
+# sort(c(1,2,3,4), decreasing = TRUE)   Sorting
+# is.*                                  Check datatype
+# as.*                                  Change datatype
+# unlist                                List to vector
+# append                                Append list
+# rev                                   Reverse
 
 # %% Help -------------------------------------------
 
@@ -132,11 +142,76 @@ vapply(cities, first_and_last, numberic(2)) # Error
 # More save than sapply
 
 
-# %% Function -----------------------------------------
+# %% Regular expressions ----------------------------
+?regex
 
+cities_l <- tolower(c("New York","Paris","Amsterdam","London","Cape Town","Vienna"))
 
+# grepl() --> boolean
+grepl(pattern = "a", x = cities_l)    # Search for expression, incl. a "a"
+grepl(pattern = "^a", x = cities_l)   # Search for expression, start with a "a"
+grepl(pattern = "a$", x = cities_l)   # Search for expression, ends with a "a"
 
+# grep() --> index
+grep(pattern = "a", x = cities_l)
+grep(pattern = "^a", x = cities_l)
+grep(pattern = "a$", x = cities_l)
 
+# sub() --> replace first found match
+sub(pattern = "a", replacement = 'o' , x = cities_l)
+
+# gsub() --> replace every single match
+gsub(pattern = "a", replacement = 'o' , x = cities_l)
+gsub(pattern = "a|i", replacement = '_' , x = cities_l)
+gsub(pattern = "a|i|o", replacement = '_' , x = cities_l)
+
+# Advanced example
+awards <- c("Won 1 Oscar.",
+            "Won 1 Oscar. Another 9 wins & 24 nominations.",
+            "1 win and 2 nominations.",
+            "2 wins & 3 nominations.",
+            "Nominated for 2 Golden Globes. 1 more win & 2 nominations.",
+            "4 wins & 1 nomination.")
+
+sub(".*\\s([0-9]+)\\snomination.*$", "\\1", awards)
+
+# %% Time and dates  ----------------------------
+
+# extra packages : lubridate, zoo, xts
+
+# %Y: 4-digit year (1982)
+# %y: 2-digit year (82)
+# %m: 2-digit month (01)
+# %d: 2-digit day of the month (13)
+# %A: weekday (Wednesday)
+# %a: abbreviated weekday (Wed)
+# %B: month (January)
+# %b: abbreviated month (Jan)
+
+today <- Sys.Date()
+today
+
+now <- Sys.time()
+now
+
+class(today)
+class(now)
+
+my_date <- as.Date('1995-05-24')
+my_date
+
+my_date <- as.Date('1995-24-05', format = '%Y-%d-%m')
+my_date
+
+my_time <- as.POSIXct('1995-05-24 10:01:07')
+my_time
+
+add_day <- my_date + 1
+diff_day <- my_date - add_day
+
+unclass(my_date) # date to numeric
+unclass(my_time) # time to numeric
+attr(my_time, 'tzone')
 
 
 
